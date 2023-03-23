fe43c1628c11a3482f45ff58fac3dd4b95283d28
reto ambrosini <reto.ambrosini@gmail.com>
2023-03-23 12:21:27 +0100

    pwm e led 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
 
-#define LED 13
+#define LED_PWM 13
+#define S_1 62
+#define LED_1 66
+
+int8_t duty=50;	// duty cycle in %
+int8_t valore_pwm;	// valore da inviare al pin
 
 void setup() 
 {
-	pinMode(LED,OUTPUT);
+	pinMode(LED_PWM,OUTPUT);
+	pinMode(LED_1,OUTPUT);
+	valore_pwm=duty*255/100;
+	analogWrite(LED_PWM,valore_pwm);
 }
 
 void loop() 
 {
-	digitalWrite(LED,1);
-	digitalWrite(LED,0);
+	digitalWrite(LED_1,1);
 }
